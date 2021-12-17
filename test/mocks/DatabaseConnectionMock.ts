50a5811335c76061e375b15fef1be736b396fbbf
201519040032 <dimiortt@gmail.com>
2021-12-17 05:32:42 -0300

    Created DatabaseConnectionMock

---
+++ test/mocks/DatabaseConnectionMock.ts
@@ -0,0 +1,41 @@
+import DatabaseConnection from '../../src/infra/database/DatabaseConnection';
+import { newDb } from 'pg-mem';
+
+// then use it like you would with pg-promise
+
+export default class DatabaseConnectionMock implements DatabaseConnection {
+  pgp: any;
+
+  constructor() {}
+
+  async build() {
+    const pgp = await newDb().adapters.createPgPromise();
+    await pgp.connect();
+
+    await pgp.query(`CREATE SCHEMA formfy;
+
+    
+    CREATE TABLE formfy.form (  
+      id serial primary key,
+      name text unique not null
+    );
+    
+    
+    CREATE TABLE formfy.form_field (
+      id serial primary key,
+      form_id integer not null,
+      label text not null,
+      type text not null,
+      options text
+    );`);
+
+    // colocar um TypeORM vai mudar tudo...
+
+    this.pgp = pgp;
+    return this;
+  }
+
+  query(statement: string, params?: any): Promise<any> {
+    return this.pgp.query(statement, params);
+  }
+}

42bc2aa27f49aef3bd1c8b7e46060e173ca85104
201519040032 <dimiortt@gmail.com>
2022-01-14 04:57:02 -0300

    Created DeleteRegistryInput dto

--- src/application/dto/CreateRegistryInput.ts
+++ src/application/dto/CreateRegistryInput.ts
@@ -1,5 +1,5 @@
 import { FieldValue } from './../../domain/entitites/Types';
 
 export default class CreateRegistryInput {
-  constructor(readonly formName: string, readonly formId: number, readonly values: FieldValue[]) {}
+  constructor(readonly formId: number, readonly values: FieldValue[]) {}
 }

34731e9439647663f10b9abda589b8e7cc94be88
201519040032 <dimiortt@gmail.com>
2022-01-28 17:04:03 -0300

    Created FormFieldsGateway

---
+++ client/src/infra/api/FormFieldsGateway.ts
@@ -0,0 +1,34 @@
+import { FormFieldType } from './../../domain/FormField';
+import HttpClient from '../http/HttpClient';
+
+export class FormFieldsGateway {
+  serverBaseUrl = 'http://localhost:4000/';
+  constructor(readonly httpClient: HttpClient) {}
+
+  updateFormField(
+    label: string,
+    formId: number,
+    newType: FormFieldType,
+    newLabel: string,
+    newOptions: string[]
+  ) {
+    const data = {
+      label,
+      formId,
+      newType,
+      newLabel,
+      newOptions,
+    };
+    const url = `${this.serverBaseUrl}/formField`;
+    return this.httpClient.request(url, 'PATCH', data);
+  }
+
+  deleteFormField(formId: number, fieldLabel: string) {
+    const url = `${this.serverBaseUrl}/formField`;
+    const data = {
+      formId,
+      fieldLabel,
+    };
+    return this.httpClient.request(url, 'DELETE', data);
+  }
+}

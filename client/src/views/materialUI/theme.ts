bd87b18295616969e3053b9ed00b551aafdffa17
201519040032 <dimiortt@gmail.com>
2022-01-28 16:58:28 -0300

    Started MaterialUI custom theme

---
+++ client/src/views/materialUI/theme.ts
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { createTheme } from '@mui/material';
+// import { createTheme } from '@mui/system';
+
+export const theme = createTheme({
+  palette: {
+    // background: {
+    //   paper: '#fff',
+    // },
+    // text: {
+    //   primary: '#173A5E',
+    //   secondary: '#46505A',
+    // },
+    // action: {
+    //   active: '#001E3C',
+    // },
+    // success: {
+    //   dark: '#009688',
+    // },
+    primary: {
+      main: '#21336E',
+    },
+  },
+});

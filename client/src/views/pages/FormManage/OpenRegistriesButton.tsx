fa3f91b6697806dc52cd096e160591e67aa00cfa
201519040032 <dimiortt@gmail.com>
2022-01-29 02:58:51 -0300

    Created OpenRegistriesButton

---
+++ client/src/views/pages/FormManage/OpenRegistriesButton.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useGetRegistries } from '../../../application/usecase/useGetRegistries';
+import { Button } from '@mui/material';
+import { RootState } from '../../../application/store/configureStore';
+import { useSelector } from 'react-redux';
+
+export const OpenRegistriesButton = () => {
+  const { formId }: any = useParams();
+  const { getRegistries } = useGetRegistries(parseInt(formId));
+  const registriesCount = useSelector((state: RootState) => state.registries.registries?.length);
+
+  useEffect(() => {
+    getRegistries();
+  }, []);
+
+  return (
+    <div>
+      <Button variant='contained' color='success'>
+        {' '}
+        Registries {registriesCount}
+      </Button>
+    </div>
+  );
+};

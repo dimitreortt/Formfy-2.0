e5a276656d0a318c961c392b98658adebe247741
201519040032 <dimiortt@gmail.com>
2022-02-01 04:19:12 -0300

    Created fuction isValidBRNumber() and regex to test a string against a brazilian number

--- client/src/views/components/FormPreview/utils/isValidNumber.ts
+++ client/src/views/components/FormPreview/utils/isValidNumber.ts
@@ -3,6 +3,11 @@ const isValidUSNumber = (phoneNumber: string) => {
   return usNumberRegex.test(phoneNumber);
 };
 
+const isValidBRNumber = (phoneNumber: string) => {
+  const brazilNumberRegex = /^[(]?[1-9]{2}[)]?[\s\.]?9[\s]?[0-9]{4}[-\s\.]?[0-9]{4}$/;
+  return brazilNumberRegex.test(phoneNumber);
+};
+
 const isValidPhoneNumber = (phoneNumber: string) => {
   return isValidUSNumber(phoneNumber) || isValidBRNumber(phoneNumber);
 };

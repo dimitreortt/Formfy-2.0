6d9a38965d446a4b49d4676637d77dd7e246b558
201519040032 <dimiortt@gmail.com>
2022-02-01 04:18:29 -0300

    Created fuction isValidUSNumber() and regex to test a string against an US number

--- client/src/views/components/FormPreview/utils/isValidNumber.ts
+++ client/src/views/components/FormPreview/utils/isValidNumber.ts
@@ -1,3 +1,8 @@
+const isValidUSNumber = (phoneNumber: string) => {
+  const usNumberRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+  return usNumberRegex.test(phoneNumber);
+};
+
 const isValidPhoneNumber = (phoneNumber: string) => {
   return isValidUSNumber(phoneNumber) || isValidBRNumber(phoneNumber);
 };

7940f78bfc04d541bdd845197d9eed22c14b7076
201519040032 <dimiortt@gmail.com>
2022-02-04 05:40:49 -0300

    Created usecase MoveFieldDown implementation

---
+++ server/src/application/dto/MoveFieldDownInput.ts
@@ -0,0 +1,3 @@
+export default class MoveFieldDownInput {
+  constructor(readonly formId: number, readonly fieldIndex: number) {}
+}
---
+++ server/src/application/usecase/MoveFieldDown.ts
@@ -0,0 +1,13 @@
+import FormRepository from '../../domain/repository/FormRepository';
+import MoveFieldDownInput from '../dto/MoveFieldDownInput';
+
+export default class MoveFieldDown {
+  constructor(readonly formRepository: FormRepository) {}
+
+  async execute(input: MoveFieldDownInput) {
+    const fieldsCount = await this.formRepository.formFieldsCount(input.formId);
+    if (input.fieldIndex >= fieldsCount)
+      throw new Error('Cannot move down a field with the last index');
+    await this.formRepository.swapIndexes(input.formId, input.fieldIndex, input.fieldIndex + 1);
+  }
+}

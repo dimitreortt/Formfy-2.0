8b80c3ded08a404582afe4bc8df72aaba66b291b
201519040032 <dimiortt@gmail.com>
2022-02-04 01:19:48 -0300

    Transfered FormFieldDTO to domain/ folder and updated FormDTO to also return fields: FormFieldDTO

--- server/src/domain/dto/FormDTO.ts
+++ server/src/domain/dto/FormDTO.ts
@@ -1,3 +1,5 @@
+import FormFieldDTO from './FormFieldDTO';
+
 export default class FormDTO {
-  constructor(readonly name: string, readonly id: number) {}
+  constructor(readonly name: string, readonly id: number, readonly fields: FormFieldDTO[]) {}
 }

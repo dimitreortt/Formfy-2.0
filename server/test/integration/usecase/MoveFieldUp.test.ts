efdae84c6c5ea5a4e8f57f9c0fd0c46457175937
201519040032 <dimiortt@gmail.com>
2022-02-05 01:32:23 -0300

    Created test 'Should not move a field up if it is the first one' in MoveFieldUp usecase test

--- server/test/integration/usecase/MoveFieldUp.test.ts
+++ server/test/integration/usecase/MoveFieldUp.test.ts
@@ -1,3 +1,4 @@
+import { wipeDatabaseForms } from './../api/utils/wipeDatabseForms';
 import FormField from '../../../src/domain/entitites/FormField';
 import CreateFormInput from '../../../src/application/dto/CreateFormInput';
 import CreateForm from '../../../src/application/usecase/CreateForm';
@@ -22,14 +23,20 @@ beforeAll(async () => {
   formDAO = new FormDAODatabase(databaseConnection);
 });
 
+afterEach(async () => {
+  await wipeDatabaseForms(formDAO, formRepositoryDatabase);
+});
+
 test("Should move a field up, meaning exchanging it's index with the field with previous index", async () => {
+  const firstIndex = 0;
+  const lastIndex = 1;
   const fields = [
-    new FormField('Short Text', 'Number of patches', undefined, 0),
-    new FormField('Short Text', 'Name', undefined, 1),
+    new FormField('Short Text', 'Number of patches', undefined, firstIndex),
+    new FormField('Short Text', 'Name', undefined, lastIndex),
   ];
   const createFormInput = new CreateFormInput('Stock Analysis', fields);
   const createFormOutput = await createForm.execute(createFormInput);
-  const moveFieldUpInput = new MoveFieldUpInput(createFormOutput.formId, 1);
+  const moveFieldUpInput = new MoveFieldUpInput(createFormOutput.formId, lastIndex);
   const moveFieldUpOutput = await moveFieldUp.execute(moveFieldUpInput);
   const formFields = await formDAO.getFormFields(createFormOutput.formId);
   const numberOfPatchesFormField = formFields[0];
@@ -38,4 +45,17 @@ test("Should move a field up, meaning exchanging it's index with the field with 
   expect(nameFormField.index).toBe(0);
 });
 
-// test('Should not move a field up if it is the first one', async () => {});
+test('Should not move a field up if it is the first one', async () => {
+  const firstIndex = 0;
+  const lastIndex = 1;
+  const fields = [
+    new FormField('Short Text', 'Number of patches', undefined, firstIndex),
+    new FormField('Short Text', 'Name', undefined, lastIndex),
+  ];
+  const createFormInput = new CreateFormInput('Stock Analysis', fields);
+  const createFormOutput = await createForm.execute(createFormInput);
+  const moveFieldUpInput = new MoveFieldUpInput(createFormOutput.formId, firstIndex);
+  await expect(async () => {
+    await moveFieldUp.execute(moveFieldUpInput);
+  }).rejects.toThrow();
+});

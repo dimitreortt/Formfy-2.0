6ae9059ba5d22929a3c5cff05515ef58767d85a6
201519040032 <dimiortt@gmail.com>
2022-02-06 09:34:35 -0300

    Added 'Content-Type' header in FetchAdapter

--- client/src/infra/http/FetchAdapter.ts
+++ client/src/infra/http/FetchAdapter.ts
@@ -4,7 +4,11 @@ type HttpRequestMethod = 'POST' | 'GET' | 'PATCH' | 'DELETE';
 
 export class FetchAdapter implements HttpClient {
   request(url: string, method: HttpRequestMethod, data?: object): Promise<any> {
-    const options = { method, body: data ? JSON.stringify(data) : undefined };
+    const options = { 
+      method, 
+      headers: { 'Content-Type': 'application/json' }, 
+      body: data ? JSON.stringify(data) : undefined 
+    };
     return fetch(url, options).then((res: any) => res.json());
   }
 }

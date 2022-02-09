0bb3445ba298cb30073008d60c2d85d60caf65aa
201519040032 <dimiortt@gmail.com>
2022-02-09 04:51:53 -0300

    Created test 'Should add a form field' in FormRepositoryDatabase.test suite

--- server/src/application/usecase/MoveFieldUp.ts
+++ server/src/application/usecase/MoveFieldUp.ts
@@ -1,12 +1,16 @@
-import FormRepository from '../../domain/repository/FormRepository';
-import MoveFieldUpInput from '../dto/MoveFieldUpInput';
+import FormRepository from "../../domain/repository/FormRepository";
+import MoveFieldUpInput from "../dto/MoveFieldUpInput";
 
 export default class MoveFieldUp {
   constructor(readonly formRepository: FormRepository) {}
 
   async execute(input: MoveFieldUpInput) {
-    console.log(input);
-    if (input.fieldIndex <= 0) throw new Error('Cannot move up a field with index 0');
-    await this.formRepository.swapIndexes(input.formId, input.fieldIndex, input.fieldIndex - 1);
+    if (input.fieldIndex <= 0)
+      throw new Error("Cannot move up a field with index 0");
+    await this.formRepository.swapIndexes(
+      input.formId,
+      input.fieldIndex,
+      input.fieldIndex - 1
+    );
   }
 }
--- server/test/unit/FormRepositoryDatabase.test.ts
+++ server/test/unit/FormRepositoryDatabase.test.ts
@@ -1,8 +1,10 @@
-import { wipeDatabaseForms } from './../integration/api/utils/wipeDatabseForms';
-import FormRepositoryDatabase from '../../src/infra/repository/database/FormRepositoryDatabase';
-import DatabaseConnectionMock from '../mocks/DatabaseConnectionMock';
-import FormDAO from '../../src/application/query/FormDAO';
-import FormDAODatabase from '../../src/infra/dao/FormDAODatabase';
+import { FormFieldType } from "./../../../client/src/domain/FormField";
+import { wipeDatabaseForms } from "./../integration/api/utils/wipeDatabseForms";
+import FormRepositoryDatabase from "../../src/infra/repository/database/FormRepositoryDatabase";
+import DatabaseConnectionMock from "../mocks/DatabaseConnectionMock";
+import FormDAO from "../../src/application/query/FormDAO";
+import FormDAODatabase from "../../src/infra/dao/FormDAODatabase";
+import FormField from "../../src/domain/entitites/FormField";
 
 let databaseConnection: any;
 let formRepository: FormRepositoryDatabase;
@@ -18,59 +20,64 @@ afterEach(async () => {
   await wipeDatabaseForms(formDAO, formRepository);
 });
 
-test('Should delete fields from db based on formId', async () => {
+test("Should delete fields from db based on formId", async () => {
   const FORM_ID = 1;
   const fields = [
     {
       formId: FORM_ID,
-      label: 'Color',
-      type: 'Short Text',
+      label: "Color",
+      type: "Short Text",
       index: 0,
     },
     {
       formId: FORM_ID,
-      label: 'Darkness',
-      type: 'Short Text',
+      label: "Darkness",
+      type: "Short Text",
       index: 1,
     },
   ];
   for (const field of fields) {
     await databaseConnection.query(
-      'insert into formfy.form_field (form_id, label, type, index) values ($1, $2, $3, $4);',
+      "insert into formfy.form_field (form_id, label, type, index) values ($1, $2, $3, $4);",
       [field.formId, field.label, field.type, field.index]
     );
   }
   await formRepository.deleteFields(FORM_ID);
   const fieldsData = databaseConnection.query(
-    'select * from formfy.form_field where form_id = $1;',
+    "select * from formfy.form_field where form_id = $1;",
     [FORM_ID]
   );
   expect(fieldsData).toMatchObject({});
 });
 
-test('Should return the correct count in form_field table', async () => {
+test("Should return the correct count in form_field table", async () => {
   const FORM_ID = 2;
   const field = {
     formId: FORM_ID,
-    label: 'Color',
-    type: 'Short Text',
+    label: "Color",
+    type: "Short Text",
     index: 0,
   };
   await databaseConnection.query(
-    'insert into formfy.form_field (form_id, label, type, index) values ($1, $2, $3, $4);',
+    "insert into formfy.form_field (form_id, label, type, index) values ($1, $2, $3, $4);",
     [field.formId, field.label, field.type, field.index]
   );
 
   const ANOTHER_FORM_ID = 5;
   const anotherField = {
     formId: ANOTHER_FORM_ID,
-    label: 'Darkness',
-    type: 'Short Text',
+    label: "Darkness",
+    type: "Short Text",
     index: 0,
   };
   await databaseConnection.query(
-    'insert into formfy.form_field (form_id, label, type, index) values ($1, $2, $3, $4);',
-    [anotherField.formId, anotherField.label, anotherField.type, anotherField.index]
+    "insert into formfy.form_field (form_id, label, type, index) values ($1, $2, $3, $4);",
+    [
+      anotherField.formId,
+      anotherField.label,
+      anotherField.type,
+      anotherField.index,
+    ]
   );
 
   const count1 = await formRepository.formFieldsCount(FORM_ID);
@@ -91,54 +98,50 @@ test("Should swap a field's index with the field with previous index", async () 
   const fields = [
     {
       formId: FORM_ID,
-      label: 'Color',
-      type: 'Short Text',
+      label: "Color",
+      type: "Short Text",
       index: firstFieldIndex,
     },
     {
       formId: FORM_ID,
-      label: 'Darkness',
-      type: 'Short Text',
+      label: "Darkness",
+      type: "Short Text",
       index: secondFieldIndex,
     },
   ];
   for (const field of fields) {
     await databaseConnection.query(
-      'insert into formfy.form_field (form_id, label, type, index) values ($1, $2, $3, $4);',
+      "insert into formfy.form_field (form_id, label, type, index) values ($1, $2, $3, $4);",
       [field.formId, field.label, field.type, field.index]
     );
   }
 
   await formRepository.swapIndexes(FORM_ID, secondFieldIndex, firstFieldIndex);
 
-  const [
-    fieldData,
-  ] = await databaseConnection.query(
-    'select * from formfy.form_field where form_id = $1 and index = $2;',
+  const [fieldData] = await databaseConnection.query(
+    "select * from formfy.form_field where form_id = $1 and index = $2;",
     [FORM_ID, secondFieldIndex]
   );
-  expect(fieldData.label).toBe('Color');
+  expect(fieldData.label).toBe("Color");
 
-  const [
-    fieldData2,
-  ] = await databaseConnection.query(
-    'select * from formfy.form_field where form_id = $1 and index = $2;',
+  const [fieldData2] = await databaseConnection.query(
+    "select * from formfy.form_field where form_id = $1 and index = $2;",
     [FORM_ID, firstFieldIndex]
   );
-  expect(fieldData2.label).toBe('Darkness');
+  expect(fieldData2.label).toBe("Darkness");
 });
 
-test('Should not swap a form_field with itself', async () => {
+test("Should not swap a form_field with itself", async () => {
   const FORM_ID = 4;
   const fieldIndex = 0;
   const field = {
     formId: FORM_ID,
-    label: 'Color',
-    type: 'Short Text',
+    label: "Color",
+    type: "Short Text",
     index: fieldIndex,
   };
   await databaseConnection.query(
-    'insert into formfy.form_field (form_id, label, type, index) values ($1, $2, $3, $4);',
+    "insert into formfy.form_field (form_id, label, type, index) values ($1, $2, $3, $4);",
     [field.formId, field.label, field.type, field.index]
   );
 
@@ -147,4 +150,21 @@ test('Should not swap a form_field with itself', async () => {
   }).rejects.toThrow();
 });
 
+test("Should add a form field", async () => {
+  const FORM_ID = 6;
+  const fieldType: FormFieldType = "Short Text";
+  const fieldLabel = "Name";
+  const field = new FormField(fieldType, fieldLabel);
+
+  await formRepository.addField(FORM_ID, field);
+
+  const [fieldData] = await databaseConnection.query(
+    "select * from formfy.form_field where form_id = $1;",
+    [FORM_ID]
+  );
+
+  expect(fieldData.label).toBe(fieldLabel);
+  expect(fieldData.type).toBe(fieldType);
+  expect(fieldData.index).toBe(0);
+});
 //fazer o caso de formulário não existe no GetFormTest

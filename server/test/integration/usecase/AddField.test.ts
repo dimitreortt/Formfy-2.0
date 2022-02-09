5b581d945b806a9cd5892c5b51db782eee405551
201519040032 <dimiortt@gmail.com>
2022-02-09 04:13:00 -0300

    Started new usecase test suite in server AddFormField.test and it's first test 'Should add a field'

---
+++ server/test/integration/usecase/AddField.test.ts
@@ -0,0 +1,47 @@
+import { FormFieldType } from "./../../../../client/src/domain/FormField";
+import CreateFormInput from "../../../src/application/dto/CreateFormInput";
+import GetFormInput from "../../../src/application/dto/GetFormInput";
+import FormDAO from "../../../src/application/query/FormDAO";
+import GetForm from "../../../src/application/query/GetForm";
+import CreateForm from "../../../src/application/usecase/CreateForm";
+import FormField from "../../../src/domain/entitites/FormField";
+import FormRepository from "../../../src/domain/repository/FormRepository";
+import FormDAODatabase from "../../../src/infra/dao/FormDAODatabase";
+import DatabaseConnection from "../../../src/infra/database/DatabaseConnection";
+import FormRepositoryDatabase from "../../../src/infra/repository/database/FormRepositoryDatabase";
+import DatabaseConnectionMock from "../../mocks/DatabaseConnectionMock";
+
+let formRepository: FormRepository;
+let formDAO: FormDAO;
+let databaseConnection: DatabaseConnection;
+
+beforeAll(async () => {
+  databaseConnection = await new DatabaseConnectionMock().build();
+  formRepository = new FormRepositoryDatabase(databaseConnection);
+  formDAO = new FormDAODatabase(databaseConnection);
+});
+
+test("Should add a field", async () => {
+  const createForm = new CreateForm(formRepository);
+  const createFormInput = new CreateFormInput("Subscription", []);
+  const createFormOutput = await createForm.execute(createFormInput);
+
+  const fieldType: FormFieldType = "Short Text";
+  const fieldLabel = "Name";
+  const field = new FormField(fieldType, fieldLabel);
+
+  const addFormField = new AddFormField(formRepository);
+  const addFormFieldInput = new FormFieldInput(field);
+  await addFormField.execute(addFormFieldInput);
+
+  const getForm = new GetForm(formDAO);
+  const getFormInput = new GetFormInput("Subscription");
+  const getFormOutput = await getForm.execute(getFormInput);
+
+  expect(getFormOutput.formFields).toHaveLength(1);
+  expect(getFormOutput.formFields[0].label).toBe(fieldLabel);
+  expect(getFormOutput.formFields[0].type).toBe(fieldType);
+  // add a fieldS
+  // getForm
+  // expect it has the field added
+});

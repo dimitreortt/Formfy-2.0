bc4d01c4a78f4ab65ac02c79aaf54fd30c1aae3d
201519040032 <dimiortt@gmail.com>
2022-02-16 01:13:17 -0300

    Created test 'Should call EditField usecase on set field submit' in EditField.test

--- client/src/__tests__/EditField.test.tsx
+++ client/src/__tests__/EditField.test.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { screen } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
 import { EditField } from "../views/pages/FillForm/EditField";
 import { NewFieldParams } from "../views/pages/FillForm/AddField";
 import { IFormField } from "../domain/FormField";
 import { render } from "../__testsUtils/testUtils";
+import * as editFieldUsecase from "../application/usecase/useEditField";
 
-test("Should render correctly and render the component responsible for setting the field", async () => {
+const setup = () => {
   const initialField: IFormField = {
     label: "Category",
     type: "List Selection",
@@ -13,6 +14,28 @@ test("Should render correctly and render the component responsible for setting t
     id: 1,
     index: 0,
   };
-  render(<EditField formId={1} field={initialField} />);
+  render(
+    <EditField
+      formId={1}
+      field={initialField}
+      open={true}
+      toggleOpen={() => {}}
+    />
+  );
+};
+
+test("Should render correctly and render the component responsible for setting the field", async () => {
+  setup();
   expect(screen.getByText("Field Setup")).toBeInTheDocument();
+});
+
+test.only("Should call EditField usecase on set field submit", async () => {
+  const editFieldMock = jest.fn();
+  jest.spyOn(editFieldUsecase, "useEditField").mockReturnValue({
+    editField: editFieldMock,
+  });
+  setup();
+  expect(editFieldMock).not.toHaveBeenCalled();
+  fireEvent.click(screen.getByRole("submit-button"));
+  expect(editFieldMock).toHaveBeenCalled();
 });

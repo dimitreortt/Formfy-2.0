9c74a16a91e5c20242be95b0a9ad24380038c5be
201519040032 <dimiortt@gmail.com>
2022-02-16 03:28:11 -0300

    Created useContextMock in useDeleteForm.test suite

--- client/src/__tests__/usecase/useDeleteForm.test.tsx
+++ client/src/__tests__/usecase/useDeleteForm.test.tsx
@@ -3,6 +3,20 @@ import { FetchAdapter } from "../../infra/http/FetchAdapter";
 import { useDeleteForm } from "../../application/usecase/useDeleteForm";
 import FormsGateway from "../../infra/api/FormsGateway";
 
+type UseContextType = typeof React.useContext;
+
+let realContext: UseContextType;
+let mockContext: UseContextType;
+
+beforeEach(() => {
+  realContext = React.useContext;
+  mockContext = React.useContext = jest.fn;
+});
+
+afterAll(() => {
+  React.useContext = realContext;
+});
+
 test.only("Should call formsGateway.deleteForms", async () => {
   const gatewayDeleteFormSpy = jest
     .spyOn(FormsGateway.prototype, "deleteForm")

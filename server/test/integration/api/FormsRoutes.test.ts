510b4e1b0e8b573c698b74682dedc48a4a532849
Dimitre Ortt <dimiortt@gmail.com>
2022-02-18 01:51:08 -0300

    Created test 'Should update other fields's indexes if deleted field is not the last' in DeleteField.test suite in server

--- server/test/integration/api/FormsRoutes.test.ts
+++ server/test/integration/api/FormsRoutes.test.ts
@@ -1,15 +1,15 @@
-import { wipeDatabaseForms } from './utils/wipeDatabseForms';
-import Router from '../../../src/infra/http/Router';
-import ExpressAdapter from '../../../src/infra/http/ExpressAdapter';
-import DatabaseConnectionMock from '../../mocks/DatabaseConnectionMock';
-import request from 'supertest';
-import DatabaseConnection from '../../../src/infra/database/DatabaseConnection';
-import FormRepositoryDatabase from '../../../src/infra/repository/database/FormRepositoryDatabase';
-import Form from '../../../src/domain/entitites/Form';
-import FormDAODatabase from '../../../src/infra/dao/FormDAODatabase';
-import FormField from '../../../src/domain/entitites/FormField';
-import FormRepository from '../../../src/domain/repository/FormRepository';
-import FormDAO from '../../../src/application/query/FormDAO';
+import { wipeDatabaseForms } from "./utils/wipeDatabseForms";
+import Router from "../../../src/infra/http/Router";
+import ExpressAdapter from "../../../src/infra/http/ExpressAdapter";
+import DatabaseConnectionMock from "../../mocks/DatabaseConnectionMock";
+import request from "supertest";
+import DatabaseConnection from "../../../src/infra/database/DatabaseConnection";
+import FormRepositoryDatabase from "../../../src/infra/repository/database/FormRepositoryDatabase";
+import Form from "../../../src/domain/entitites/Form";
+import FormDAODatabase from "../../../src/infra/dao/FormDAODatabase";
+import FormField from "../../../src/domain/entitites/FormField";
+import FormRepository from "../../../src/domain/repository/FormRepository";
+import FormDAO from "../../../src/application/query/FormDAO";
 
 let router: Router;
 let databaseConnection: DatabaseConnection;
@@ -29,98 +29,112 @@ afterEach(async () => {
   await wipeDatabaseForms(formDAO, formRepository);
 });
 
-test('Should create a form', async () => {
+test("Should create a form", async () => {
   const body = {
-    name: 'Seminary',
+    name: "Seminary",
     fields: [
       {
-        type: 'Date',
-        label: 'Started At',
+        type: "Date",
+        label: "Started At",
       },
       {
-        type: 'Checkbox',
-        label: 'Topics Studied',
-        options: ['Anatomy', 'Stress', 'Faith', 'Creed'],
+        type: "Checkbox",
+        label: "Topics Studied",
+        options: ["Anatomy", "Stress", "Faith", "Creed"],
       },
     ],
   };
-  const res = await app.post('/form').send(body);
+  const res = await app.post("/form").send(body);
   expect(res.statusCode).toEqual(201);
-  expect(typeof res.body.formId).toBe('number');
-  expect(res.body.name).toBe('Seminary');
+  expect(typeof res.body.formId).toBe("number");
+  expect(res.body.name).toBe("Seminary");
 
-  const formData = await formDAO.getForm('Seminary');
+  const formData = await formDAO.getForm("Seminary");
   expect(formData.id).toBe(res.body.formId);
 });
 
-test('Should not create a form', async () => {
-  const body = { name: 'Seminary' };
-  const res = await app.post('/form').send(body);
+test("Should not create a form", async () => {
+  const body = { name: "Seminary" };
+  const res = await app.post("/form").send(body);
   expect(res.statusCode).toBe(400);
-  expect(res.body).toBe('Error!! List of form fields cannot be empty');
+  expect(res.body).toMatchObject({
+    message: "Error!! List of form fields cannot be empty",
+  });
 });
 
-test('Should update a form name', async () => {
-  const form = new Form('Subscription');
+test("Should update a form name", async () => {
+  const form = new Form("Subscription");
   const createFormOutput = await formRepository.save(form);
 
   const requestBody = {
-    name: 'Subscription',
-    newName: 'Performance',
+    name: "Subscription",
+    newName: "Performance",
   };
-  const res = await app.patch('/form').send(requestBody);
+  const res = await app.patch("/form").send(requestBody);
   expect(res.statusCode).toBe(204);
 
-  const formData = await formDAO.getForm('Performance');
+  const formData = await formDAO.getForm("Performance");
   expect(formData.id).toBe(createFormOutput.id);
 });
 
-test('Should get a form', async () => {
-  const form = new Form('Subscription');
-  form.addField(new FormField('Long Text', 'Address'));
+test("Should get a form", async () => {
+  const form = new Form("Subscription");
+  form.addField(new FormField("Long Text", "Address"));
   const createFormOutput = await formRepository.save(form);
 
   const body = {
-    name: 'Subscription',
+    name: "Subscription",
   };
 
-  const res = await app.get('/form').send(body);
+  const res = await app.get("/form").send(body);
   expect(res.statusCode).toEqual(200);
   expect(res.body.form.id).toBe(createFormOutput.id);
   expect(res.body.formFields).toBeInstanceOf(Array);
   expect(res.body.formFields).toHaveLength(1);
-  expect(res.body.formFields[0].label).toBe('Address');
+  expect(res.body.formFields[0].label).toBe("Address");
 });
 
-test('Should get the forms', async () => {
-  const form1 = new Form('Environment Management Map');
-  form1.addField(new FormField('Short Text', 'Area Code'));
-  form1.addField(new FormField('Date', 'Last review'));
-  const form2 = new Form('Land Withdraw');
-  form2.addField(new FormField('Date', 'Monitoring Starting Date'));
-  form2.addField(new FormField('List Selection', 'Land Type', ['Tundra', 'Desert', 'Grasslands']));
+test("Should get the forms", async () => {
+  const form1 = new Form("Environment Management Map");
+  form1.addField(new FormField("Short Text", "Area Code"));
+  form1.addField(new FormField("Date", "Last review"));
+  const form2 = new Form("Land Withdraw");
+  form2.addField(new FormField("Date", "Monitoring Starting Date"));
+  form2.addField(
+    new FormField("List Selection", "Land Type", [
+      "Tundra",
+      "Desert",
+      "Grasslands",
+    ])
+  );
   await formRepository.save(form1);
   await formRepository.save(form2);
 
-  const res = await app.get('/forms').send();
+  const res = await app.get("/forms").send();
   expect(res.body.forms).toHaveLength(2);
   expect(res.body.forms[0].formFields).toHaveLength(2);
   expect(res.body.forms[1].formFields).toHaveLength(2);
 });
 
-test('Should delete form', async () => {
-  const form = new Form('Land Withdraw');
-  form.addField(new FormField('Date', 'Monitoring Starting Date'));
-  form.addField(new FormField('List Selection', 'Land Type', ['Tundra', 'Desert', 'Grasslands']));
+test("Should delete form", async () => {
+  const form = new Form("Land Withdraw");
+  form.addField(new FormField("Date", "Monitoring Starting Date"));
+  form.addField(
+    new FormField("List Selection", "Land Type", [
+      "Tundra",
+      "Desert",
+      "Grasslands",
+    ])
+  );
   const formData1 = await formRepository.save(form);
 
   const body = {
     formId: formData1.id,
   };
-  const res = await app.delete('/form').send(body);
+  const res = await app.delete("/form").send(body);
   expect(res.statusCode).toEqual(204);
 
-  const formData2 = await formDAO.getForm('Land Withdraw');
+  const formData2 = await formDAO.getForm("Land Withdraw");
   const formFieldsData = await formDAO.getFormFields(formData1.id);
   expect(formData2).toBeUndefined();
   expect(formFieldsData).toEqual([]);
--- server/test/integration/usecase/DeleteField.test.ts
+++ server/test/integration/usecase/DeleteField.test.ts
@@ -1,13 +1,13 @@
-import CreateFormInput from '../../../src/application/dto/CreateFormInput';
-import CreateForm from '../../../src/application/usecase/CreateForm';
-import FormRepositoryDatabase from '../../../src/infra/repository/database/FormRepositoryDatabase';
-import DatabaseConnectionMock from '../../mocks/DatabaseConnectionMock';
-import FormField from '../../../src/domain/entitites/FormField';
-import GetForm from '../../../src/application/query/GetForm';
-import FormDAODatabase from '../../../src/infra/dao/FormDAODatabase';
-import GetFormInput from '../../../src/application/dto/GetFormInput';
-import DeleteFieldInput from '../../../src/application/dto/DeleteFieldInput';
-import DeleteField from '../../../src/application/usecase/DeleteField';
+import CreateFormInput from "../../../src/application/dto/CreateFormInput";
+import CreateForm from "../../../src/application/usecase/CreateForm";
+import FormRepositoryDatabase from "../../../src/infra/repository/database/FormRepositoryDatabase";
+import DatabaseConnectionMock from "../../mocks/DatabaseConnectionMock";
+import FormField from "../../../src/domain/entitites/FormField";
+import GetForm from "../../../src/application/query/GetForm";
+import FormDAODatabase from "../../../src/infra/dao/FormDAODatabase";
+import GetFormInput from "../../../src/application/dto/GetFormInput";
+import DeleteFieldInput from "../../../src/application/dto/DeleteFieldInput";
+import DeleteField from "../../../src/application/usecase/DeleteField";
 
 let databaseConnection: any;
 let formRepositoryDatabase: FormRepositoryDatabase;
@@ -23,15 +23,67 @@ beforeAll(async () => {
   deleteField = new DeleteField(formRepositoryDatabase);
 });
 
-test('Should delete a form field', async () => {
-  const field = new FormField('List Selection', 'Color', ['Yellow', 'Green', 'Blue']);
-  const createFormInput = new CreateFormInput('Paint', [field]);
+test("Should delete a form field", async () => {
+  const field = new FormField("List Selection", "Color", [
+    "Yellow",
+    "Green",
+    "Blue",
+  ]);
+  const createFormInput = new CreateFormInput("Paint", [field]);
   const createFormOutput = await createForm.execute(createFormInput);
-  const deleteFieldInput = new DeleteFieldInput(createFormOutput.formId, 'Color');
+  const deleteFieldInput = new DeleteFieldInput(
+    createFormOutput.formId,
+    "Color"
+  );
   await deleteField.execute(deleteFieldInput);
-  const getFormInput = new GetFormInput('Paint');
+  const getFormInput = new GetFormInput("Paint");
   const getFormOutput = await getForm.execute(getFormInput);
   expect(getFormOutput.formFields.length).toBe(0);
+});
+
+test("Should update other fields's indexes if deleted field is not the last", async () => {
+  const FIRST_INDEX = 0;
+  const SECOND_INDEX = 1;
+  const THIRD_INDEX = 2;
+  const field1 = new FormField(
+    "Short Text",
+    "Number of patches",
+    undefined,
+    FIRST_INDEX
+  );
+  const field2 = new FormField("Short Text", "Name", undefined, SECOND_INDEX);
+  const field3 = new FormField(
+    "List Selection",
+    "Color",
+    ["Yellow", "Green", "Blue"],
+    THIRD_INDEX
+  );
+  const fields = [field1, field2, field3];
+  const createFormInput = new CreateFormInput("Stock Analysis", fields);
+  const createFormOutput = await createForm.execute(createFormInput);
+  const deleteFieldInput = new DeleteFieldInput(
+    createFormOutput.formId,
+    field1.label
+  );
+  await deleteField.execute(deleteFieldInput);
+  const getFormInput = new GetFormInput("Stock Analysis");
+  const getFormOutput = await getForm.execute(getFormInput);
+  expect(getFormOutput.formFields.length).toBe(2);
+  console.log(getFormOutput.formFields);
+  expect(
+    getFormOutput.formFields.find((field) => field.label === field2.label)
+  ).toMatchObject(
+    expect.objectContaining({
+      index: FIRST_INDEX,
+    })
+  );
+  expect(
+    getFormOutput.formFields.find((field) => field.label === field3.label)
+  ).toMatchObject(
+    expect.objectContaining({
+      index: SECOND_INDEX,
+    })
+  );
 });
 
 // fazer o caso de formulário não existe no GetFormTest

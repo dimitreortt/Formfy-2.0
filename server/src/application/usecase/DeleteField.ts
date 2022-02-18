8da3373d8ab097feb905f8b439a852bcb9381089
Dimitre Ortt <dimiortt@gmail.com>
2022-02-18 01:50:24 -0300

    Refactored DelteField usecase in server to update the rest of the fields's indexes

--- server/src/application/usecase/DeleteField.ts
+++ server/src/application/usecase/DeleteField.ts
@@ -1,10 +1,27 @@
-import FormRepository from '../../domain/repository/FormRepository';
-import DeleteFieldInput from '../dto/DeleteFieldInput';
+import FormRepository from "../../domain/repository/FormRepository";
+import DeleteFieldInput from "../dto/DeleteFieldInput";
 
 export default class DeleteField {
   constructor(private formRepository: FormRepository) {}
 
   async execute(input: DeleteFieldInput): Promise<void> {
-    await this.formRepository.deleteField(input.formId, input.label);
+    const fieldCount = await this.formRepository.formFieldsCount(input.formId);
+
+    const deletedField = await this.formRepository.deleteField(
+      input.formId,
+      input.label
+    );
+
+    const lastFieldIndex = fieldCount - 1;
+
+    let iterator = deletedField.index;
+    while (iterator < lastFieldIndex) {
+      await this.formRepository.updateFieldIndex(
+        input.formId,
+        iterator + 1,
+        iterator
+      );
+      iterator++;
+    }
   }
 }
